5eea2c7500d9fef76180f45a789ecb1692bcd407
Anh Mac <anh@bespokify.com>
2021-07-25 18:54:38 +0700

    feat: first commit

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const domain = 'localhost';
+export const protocol = 'http';
+
+export const websiteUrl = `${protocol}://${domain}:3000`;
+
+export const siteName = 'Mac Van Anh';
---
+++ src/lib/orgToHtml.ts
@@ -0,0 +1,85 @@
+import { Plugin } from 'unified';
+import { VFile } from 'vfile';
+import visit from 'unist-util-visit';
+import retext from 'retext';
+import smartypants from 'retext-smartypants';
+
+import orgParse from 'uniorg-parse';
+import org2rehype from 'uniorg-rehype';
+import { uniorgSlug } from 'uniorg-slug';
+import json from '@/lib/unified-json';
+import { visitIds } from 'orgast-util-visit-ids';
+
+const processor = json()
+  .use(orgParse)
+  .use(saveKeywords)
+  .use(removeCards)
+  .use(orgSmartypants as Plugin<any>, { dashes: 'oldschool' })
+  .use(uniorgSlug)
+  .use(saveIds)
+  .use(org2rehype);
+
+export default async function orgToHtml(file: VFile): Promise<VFile> {
+  return await processor.process(file);
+}
+
+function saveIds() {
+  return transformer;
+
+  function transformer(tree: any, file: any) {
+    const data = file.data || (file.data = {});
+    const ids = data.ids || (data.ids = {});
+
+    visitIds(tree, (id, node) => {
+      if (node.type === 'org-data') {
+        ids[id] = '';
+      } else if (node.type === 'headline') {
+        if (!node.data?.hProperties?.id) {
+          // The headline doesn't have an html id assigned.
+          //
+          // Assign an html id property based on org id property, so
+          // the links won't be broken.
+          node.data = node.data || {};
+          node.data.hProperties = node.data.hProperties || {};
+          node.data.hProperties.id = id;
+        }
+
+        ids[id] = '#' + node.data?.hProperties?.id;
+      }
+    });
+  }
+}
+
+function saveKeywords() {
+  return transformer;
+
+  function transformer(tree: any, file: any) {
+    visit(tree, 'keyword', (kw: any) => {
+      file.data[kw.key.toLowerCase()] = kw.value;
+    });
+  }
+}
+
+function removeCards() {
+  return transformer;
+
+  function transformer(tree: any) {
+    tree.children = tree.children.filter((child: any) => {
+      return !(
+        child.type === 'headline' &&
+        child.title[0]?.value?.toLowerCase() === 'cards'
+      );
+    });
+  }
+}
+
+function orgSmartypants(options: any) {
+  const processor = retext().use(smartypants, options);
+  return transformer;
+
+  function transformer(tree: any) {
+    visit(tree, 'text', (node) => {
+      node.value = String(processor.processSync(node.value));
+    });
+  }
+}
